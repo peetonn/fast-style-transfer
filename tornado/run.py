1e042e60704a09490dfa939dfa4ac854f47658bb
peetonn <peter@remap.ucla.edu>
2018-03-10 08:13:28 -0800

    style transfer service v1

--- tornado/run.py
+++ tornado/run.py
@@ -1,12 +1,13 @@
 from __future__ import print_function
 import os, sys, signal
+sys.path.insert(0, 'src')
 
 # adding import path for the directory above this sctip (for deeplab modules)
 myPath = os.path.dirname(sys.argv[0])
 rootPath = os.path.join(myPath,'..')
 uploadPath =  os.path.join(rootPath, "upload")
 resultsPath = os.path.join(rootPath, "results")
-weightsModelPath = os.path.join(rootPath, "deeplab_resnet.ckpt")
+modelsDir = os.path.join(rootPath, 'ce-models');
 
 sys.path.append(rootPath)
 
@@ -22,15 +23,22 @@ import datetime
 from PIL import Image
 import tensorflow as tf
 import numpy as np
+import transform, numpy as np, vgg, pdb, os
+import scipy.misc
+import tensorflow as tf
+from utils import save_img, get_img, exists, list_files
+from collections import defaultdict
+import time
+import json
+import subprocess
+import numpy
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import moviepy.video.io.ffmpeg_writer as ffmpeg_writer
 
-from deeplab_resnet import DeepLabResNetModel, ImageReader, decode_labels, dense_crf, prepare_label
+BATCH_SIZE = 4
+DEVICE = '/gpu:0'
 
-SAVE_DIR = './output/'
-IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
-GPU=1
-###
-
-port = 8888
+port = 8889
 ipaddress = "131.179.142.7"
 hostUrl = "http://"+ipaddress+":"+str(port)
 define("port", default=port, help="run on the given port", type=int)
@@ -38,6 +46,7 @@ define("port", default=port, help="run on the given port", type=int)
 quit = False
 requestQueue = Queue()
 
+#******************************************************************************
 def timestampMs():
     return int((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds() * 1000)
 
@@ -71,102 +80,102 @@ class UploadHandler(tornado.web.RequestHandler):
 
         self.finish(hostUrl+"/result/"+fileID+".png")
 
-### DEEPLAB STUFF BELOW
-def load(saver, sess, ckpt_path):
-    '''Load trained weights.
-    
-    Args:
-      saver: TensorFlow saver object.
-      sess: TensorFlow session.
-      ckpt_path: path to checkpoint file with parameters.
-    ''' 
-    saver.restore(sess, ckpt_path)
-    print("Restored model parameters from {}".format(ckpt_path))
-
-def deeplabProcessing(gpuId):
-    """Create the model and start the evaluation process."""
-    print("Starting worker on GPU "+str(gpuId) + "...")
+def fstWorker(sampleImg, checkpoint_dir, device_t='/gpu:0'):
+    modelName = os.path.basename(checkpoint_dir)
+    print("Starting worker with model "+modelName+" on GPU "+device_t + "...")
 
     def printWorker(msg):
-        print(str(timestampMs())+" [gpu-worker-"+ str(gpuId)+"] " + msg)
+         print(str(timestampMs())+" [gpu-worker-"+ modelName+"] " + msg)
+    img_shape = get_img(sampleImg).shape
 
-    printWorker("Waiting for segmentation requests...")
-    initialized = False
-    while (not quit):
-        fileId = requestQueue.get() # will block
-        if fileId == "quit"+str(gpuId):
-            printWorker("Received quit command")
-            break
+    g = tf.Graph()
+    batch_size = 1 # min(len(paths_out), batch_size)
+    curr_num = 0
+    soft_config = tf.ConfigProto(allow_soft_placement=True)
+    soft_config.gpu_options.allow_growth = True
+    with g.as_default(), g.device(device_t), \
+            tf.Session(config=soft_config) as sess:
+        batch_shape = (batch_size,) + img_shape
+        img_placeholder = tf.placeholder(tf.float32, shape=batch_shape,
+                                         name='img_placeholder')
 
-        printWorker("Received request for DL segmentaiton: "+fileId)
+        preds = transform.net(img_placeholder)
+        saver = tf.train.Saver()
+        if os.path.isdir(checkpoint_dir):
+            ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+            else:
+                raise Exception("No checkpoint found...")
+        else:
+            saver.restore(sess, checkpoint_dir)
 
-        t1 = datetime.datetime.now()
+        while not quit:
+            printWorker("Waiting for requests...")
+            fileId = requestQueue.get()
+            if fileId == "quit-"+modelName:
+                printWorker("Received quit command")
+                break
+            
+            t1 = timestampMs()
+            printWorker("Received request for style transfer: "+fileId)
+            path = os.path.join(uploadPath, fileId)
 
-        imgPath = os.path.join(uploadPath, fileId)
-        # Prepare image.
-        imgRGB = tf.image.decode_jpeg(tf.read_file(imgPath), channels=3)
-        # Convert RGB to BGR.
-        img_r, img_g, img_b = tf.split(imgRGB, 3, axis=2)
-        imgBGR = tf.cast(tf.concat([img_b, img_g, img_r], 2), dtype=tf.float32)
-        # Extract mean.
-        imgBGR -= IMG_MEAN 
+            printWorker("Reading image "+path)
+            img = get_img(path)
 
-        printWorker("Will create network")
-        # Create network.
-        net = DeepLabResNetModel({'data': tf.expand_dims(imgBGR, dim=0)}, is_training=False)
-        tf.get_variable_scope().reuse_variables()
-    
-        printWorker("Network created")
+            printWorker("Running style transfer...")
+            X = np.zeros(batch_shape, dtype=np.float32)
+            X[0] = img
+            _preds = sess.run(preds, feed_dict={img_placeholder:X})
 
-        # Which variables to load.
-        restore_var = tf.global_variables()
+            pathOut = os.path.join(resultsPath, fileId+".png")
+            save_img(pathOut, _preds[0])
+            t2 = timestampMs()
 
-        # Predictions.
-        raw_output = net.layers['fc1_voc12']
-        raw_output_up = tf.image.resize_bilinear(raw_output, tf.shape(imgBGR)[0:2,])
-    
-        printWorker("Predictions")
+            printWorker("Saved result at "+pathOut)
+            printWorker("Processing took "+str(t2-t1)+"ms")
 
-        # CRF.
-        raw_output_up = tf.nn.softmax(raw_output_up)
-        raw_output_up = tf.py_func(dense_crf, [raw_output_up, tf.expand_dims(imgRGB, dim=0)], tf.float32)
-    
-        printWorker("CRF")
+    printWorker("Completed")
 
-        raw_output_up = tf.argmax(raw_output_up, dimension=3)
-        pred = tf.expand_dims(raw_output_up, dim=3)
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--checkpoint', type=str,
+                        dest='checkpoint_dir',
+                        help='dir or .ckpt file to load checkpoint from',
+                        metavar='CHECKPOINT', required=True)
 
-        if not initialized:
-            printWorker("Setup tf session")
-            # Set up TF session and initialize variables. 
-            config = tf.ConfigProto(device_count = {'GPU': gpuId})
-            config.gpu_options.allow_growth = True
-            sess = tf.Session(config=config)
-            init = tf.global_variables_initializer()
-    
-            sess.run(init)
-            printWorker("TF session initialized")
+    parser.add_argument('--in-path', type=str,
+                        dest='in_path',help='dir or file to transform',
+                        metavar='IN_PATH', required=True)
 
-            # Load weights.
-            loader = tf.train.Saver(var_list=restore_var)
-            load(loader, sess, weightsModelPath)
+    help_out = 'destination (dir or file) of transformed file or files'
+    parser.add_argument('--out-path', type=str,
+                        dest='out_path', help=help_out, metavar='OUT_PATH',
+                        required=True)
 
-            initialized = True
-    
-        # Perform inference.
-        preds = sess.run(pred)
-    
-        msk = decode_labels(preds)
-        im = Image.fromarray(msk[0])
-        maskPath = os.path.join(resultsPath, fileId)+".png"
-        im.save(maskPath)
-        
-        originalFile = os.path.join(uploadPath,fileId)
-        os.remove(originalFile)
+    parser.add_argument('--device', type=str,
+                        dest='device',help='device to perform compute on',
+                        metavar='DEVICE', default=DEVICE)
 
-        t2 = datetime.datetime.now()
-        printWorker("Processing took "+str((t2-t1).microseconds)+"usec. Result is at "+maskPath)
+    parser.add_argument('--batch-size', type=int,
+                        dest='batch_size',help='batch size for feedforwarding',
+                        metavar='BATCH_SIZE', default=BATCH_SIZE)
 
+    parser.add_argument('--allow-different-dimensions', action='store_true',
+                        dest='allow_different_dimensions', 
+                        help='allow different image dimensions')
+
+    return parser
+
+def check_opts(opts):
+    exists(opts.checkpoint_dir, 'Checkpoint not found!')
+    exists(opts.in_path, 'In path not found!')
+    if os.path.isdir(opts.out_path):
+        exists(opts.out_path, 'out dir not found!')
+        assert opts.batch_size > 0
+
+####
 def signal_handler(signum, frame):
     global is_closing
     print('Received stop signal, exiting...')
@@ -175,14 +184,20 @@ def signal_handler(signum, frame):
 
 def main():
     signal.signal(signal.SIGINT, signal_handler)
+    models = os.listdir(modelsDir)
 
     # TODO: this can be expanded to utilize more than one GPU
     nGpus = 1
-    workers = []
-    for i in range(0,nGpus):
-        worker = Thread(target=deeplabProcessing, args=(i,))
+    workers = {}
+    nWorkers = 1 #len(models)
+    # sampleImg = os.path.join(rootPath, 'sample840x560.jpg')
+    sampleImg = os.path.join(rootPath, 'sample420x280.jpg')
+    for i in range(0,nWorkers):
+        modelName = models[i]
+        checkpoint = os.path.join(modelsDir, modelName)
+        worker = Thread(target=fstWorker, args=(sampleImg, checkpoint, ))
         worker.start()
-        workers.append(worker)
+        workers[modelName] = worker
 
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
@@ -190,8 +205,8 @@ def main():
     
     print("Will terminate GPU workers...")
 
-    for i in range(0,len(workers)):
-        requestQueue.put("quit"+str(i))
+    for k in workers:
+        requestQueue.put("quit-"+str(k))
 
 if __name__ == "__main__":
     main()
